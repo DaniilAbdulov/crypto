2315010ddef9731a032e355030a92b79dc237e53
Daniil Abdulov <abdulov.dany@yandex.ru>
2026-05-31 14:06:41 +0500

    -

--- packages/pg/src/knex.ts
+++
@@ -1,5 +0,0 @@
-import knex, {Knex} from 'knex';
-
-export function createDb(config: Knex.Config) {
-  return knex(config);
-}
--- packages/ts/types/users.d.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-  uuid: string;
-  password_hash: string;
-  name: string;
-  created_at: string;
-}
